f5ee68cd99dacdd4790d1cbdd04005cdd9b9ac91
Lokesh Vashishtha <krlokeshv99@gmail.com>
2020-07-08 17:10:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# TIC-TAC-TOE
+
+ This is a tic-tac-toe game implemented in python, which can be played by two players.
+
+ I've developed this with the help of Youtube Tutorial Videos by [TokyoEdTech](https://www.youtube.com/channel/UC2vm-0XX5RkWCXWwtBZGOXg)
+
+ Sooner, A 'v/s computer' tic-tac-toe will also be added 😉.
+
+ Since, I consider `C++` as my primary programming language, I'll try to code the same with `C++`, without videos.
---
+++ tictactoe-two_player.py
@@ -0,0 +1,136 @@
+import os
+
+os.system('cls')  # to clear cmd screen, use 'clear' if 'cls doesn't work
+
+
+class Board:
+    def __init__(self):
+        self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]  # initialize the empty board
+
+    def display(self):
+        print(" {} | {} | {} ".format(self.cells[1], self.cells[2], self.cells[3]) + " \t{} | {} | {} ".format(1, 2, 3))
+        print(" --------- " + " \t--------- ")
+        print(" {} | {} | {} ".format(self.cells[4], self.cells[5], self.cells[6]) + " \t{} | {} | {} ".format(4, 5, 6))
+        print(" --------- " + " \t--------- ")
+        print(" {} | {} | {} ".format(self.cells[7], self.cells[8], self.cells[9]) + " \t{} | {} | {} ".format(7, 8, 9))
+
+    def update_cell(self, cell_no, player):     # update the board with player
+        if self.cells[cell_no] == " ":
+            self.cells[cell_no] = player
+
+    def is_illegal(self, cell_no):      # check if the move is acceptable or not
+        if cell_no not in range(1, 10):
+            return True
+        return False
+
+    def not_empty(self, cell_no):       # check for the availability of cell
+        if self.cells[cell_no] == " ":
+            return False
+        return True
+
+    def is_winner(self, player):       # check for winner, in the possible 8 combinations
+        for combo in [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]:
+            result = True
+            for cell_no in combo:
+                if self.cells[cell_no] != player:
+                    result = False       # if board is not in any above combination, return false, game carries on
+            if result:
+                return True
+        return False
+
+    def is_tie(self):       # check for tie
+        used_cells = 0
+        for cell in self.cells:
+            if cell != " ":
+                used_cells += 1
+        if used_cells >= 9:
+            return True       # if all cells are used up, return true
+        else:
+            return False
+
+    def reset(self):       # resets the board to initial position, for new game
+        self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+if __name__ == '__main__':
+    board = Board()       # object of Board class is created
+
+    def print_header():
+        print("\n WELCOME TO TIC-TAC-TOE \n\n")
+
+    def refresh_screen():       # clears the screen for next player's input
+        # clear screen
+        os.system('cls')
+        # print header
+        print_header()
+        # show the board
+        board.display()
+
+
+    while True:
+
+        refresh_screen()
+
+        # get input from 'O'
+        o_choice = int(input("\n Player O) Choose between < 1 - 9 >: "))
+        while board.is_illegal(o_choice):       # check if move is correct
+            o_choice = int(input("\n Player O) Wrong Choice \n Please choose between < 1 - 9 >: "))
+        while board.not_empty(o_choice):       # check if move is available
+            o_choice = int(input("\n Player O) Wrong Choice {} is already occupied".format(o_choice) +
+                                 "\n Please choose another between < 1 - 9 >: "))
+
+        # update board
+        board.update_cell(o_choice, "O")
+        # check for O's win
+        if board.is_winner("O"):
+            refresh_screen()
+            print("\n 'O' WINS!! \n")
+            play_again = input(" Would you like to play again( Y / N ): ").upper()       # if player want to continue
+            if play_again == 'Y':
+                board.reset()       # reset to initial position
+                continue
+            else:
+                break
+        # check for tie
+        if board.is_tie():
+            refresh_screen()
+            print("\n TIE!! \n")
+            play_again = input(" Would you like to play again( Y / N ): ").upper()       # if player want to continue
+            if play_again == 'Y':
+                board.reset()       # reset to initial position
+                continue
+            else:
+                break
+
+
+        refresh_screen()
+        # get X input
+        x_choice = int(input("\n Player X) Choose between < 1 - 9 >: "))
+        while board.is_illegal(x_choice):       # check if move is correct
+            x_choice = int(input("\n Player X) Wrong Choice \nPlease choose between < 1 - 9 >: "))
+        while board.not_empty(x_choice):       # check if move is available
+            x_choice = int(input("\n Player X) Wrong Choice {} is already occupied".format(x_choice) +
+                                 "\n Please choose another between < 1 - 9 >: "))
+
+        # update board
+        board.update_cell(x_choice, "X")
+        # check for X's win
+        if board.is_winner("X"):
+            refresh_screen()
+            print("\n 'X' WINS!! \n")
+            play_again = input(" Would you like to play again( Y / N ): ").upper()       # if player want to continue
+            if play_again == 'Y':
+                board.reset()       # reset to initial position
+                continue
+            else:
+                break
+        # check for tie
+        if board.is_tie():
+            refresh_screen()
+            print("\n TIE!! \n")
+            play_again = input(" Would you like to play again( Y / N ): ").upper()       # if player want to continue
+            if play_again == 'Y':
+                board.reset()       # reset to initial position
+                continue
+            else:
+                break
